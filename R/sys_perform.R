9434bb7436f53a058d659bd1bed232cdb58c7385
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-06-30 13:48:35 -0400

    updating sys_perform

--- R/sys_perform.R
+++ R/sys_perform.R
@@ -2,53 +2,61 @@
 #' 
 #' This function returns the time it takes to run the following: a big matrix
 #' multiplication, a loop without pre-allocated mem, a loop with 
-#' pre-allocated memory, and writing a csv to file.  All but the file write 
-#' can be tried on mulitple cores.  As I am not a computer scientist, I have
-#' no idea if these test are meaningful.  Just things I do a lot and wanted 
-#' to see if they work faster on different machines.
+#' pre-allocated memory, and writing a csv to file. As I am not a computer 
+#' scientist, I have no idea if these test are meaningful.  Just things I do a 
+#' lot and wanted to see if they work faster on different machines. 
 #' 
-#' @param n the number of times to run the test
-#' @param cores the number of cores to test it on
+#' @param n numeric value indicating the number of times to run the test.  Default is 10
+#' @param addTo for those on EPA network a boolean indicating wether you want 
+#'        the results added to shared csv file. Default is FALSE
 #' 
 #' @export
-sys_perform<-function(n=10, cores=1){
-  mm<-vector("numeric",n)
-  l1<-vector("numeric",n)
-  l2<-vector("numeric",n)
-  wr<-vector("numeric",n)
-  for(i in 1:n){
-  
-    #Matrix Multiply Timining
-    mm[i]<-system.time(matrix(rnorm(1000000),1000)%*%matrix(rnorm(1000000),1000))[3]
-    
-    #Unallocated Loop Timing
-    x<-vector()
-    l1[i]<-system.time(
-      for(j in 1:50000)
-      {x[j]<-mean(rnorm(10))}
-    )[3]
-    
-    #Allocated Loop Timing
-    x<-vector("numeric",50000)
-    l2[i]<-system.time(
-      for(j in 1:50000)
-      {x[j]<-mean(rnorm(10))}
-    )[3]
-    
-    #Write csv timing
-    wr[i]<-system.time(write.csv(data.frame(rnorm(100000),rnorm(100000)),"test.csv"))[3]
-    file.remove("test.csv")
-  }
-
-  return(list(params=c(n=n,cores=cores),
-              matrix.multiply=mean(mm),
-              unallocated.loop=mean(l1),
-              allocated.loop=mean(l2),
-              write.csv=mean(wr),
-              total.time=sum(mean(mm),
-                             mean(l1),
-                             mean(l2),
-                             mean(wr))))
-  
-  
-}   
+sys_perform <- function(n = 10, addTo = FALSE) {
+    mm <- vector("numeric", n)
+    l1 <- vector("numeric", n)
+    l2 <- vector("numeric", n)
+    wr <- vector("numeric", n)
+    for (i in 1:n) {
+        
+        # Matrix Multiply Timining
+        mm[i] <- system.time(matrix(rnorm(1e+06), 1000) %*% matrix(rnorm(1e+06), 1000))[3]
+        
+        # Unallocated Loop Timing
+        x <- vector()
+        l1[i] <- system.time(for (j in 1:50000) {
+            x[j] <- mean(rnorm(10))
+        })[3]
+        
+        # Allocated Loop Timing
+        x <- vector("numeric", 50000)
+        l2[i] <- system.time(for (j in 1:50000) {
+            x[j] <- mean(rnorm(10))
+        })[3]
+        
+        # Write csv timing
+        wr[i] <- system.time(write.csv(data.frame(rnorm(1e+05), rnorm(1e+05)), "test.csv"))[3]
+        file.remove("test.csv")
+    }
+    if (Sys.info()["sysname"] == "Linux") {
+        mem <- strsplit(system("cat /proc/meminfo", T)[1], " ")[[1]]
+        mem <- round(as.numeric(mem[(length(mem) - 1)])/1048576)
+    } else if (Sys.info()["sysname"] == "Windows") {
+        mem <- system("wmic OS get TotalVisibleMemorySize /Value", T)
+        mem <- mem[grep("TotalVisibleMemorySize", mem)]
+        mem <- round(as.numeric(gsub("[a-zA-Z=\r]", "", mem))/1048576)
+    }
+    out<-list(User.Name = Sys.info()["user"], R.version = R.version$version.string, Machine.Name = Sys.info()["nodename"], 
+              OS = paste(Sys.info()["sysname"], Sys.info()["release"]), Memory.GB = mem, Drive = getwd(), Number.Runs = n, matrix.multiply = mean(mm), 
+              unallocated.loop = mean(l1), allocated.loop = mean(l2), write.csv = mean(wr), total.time = sum(mean(mm), mean(l1), mean(l2), 
+                                                                                                             mean(wr)))
+    if(addTo){
+      thefile<-"//a2626xrith008.nar.epa.gov/data/rSysPerform/epaRSysPerformance.csv"
+      thefile<-"smb://a2626xrith008.nar.epa.gov/data/rSysPerform/epaRSysPerformance.csv"
+      if(file.exists(thefile)){
+        write.csv(out,thefile,row.names=FALSE)
+      } else {
+        write.table(out,thefile,row.names=FALSE,col.names=FALSE,append=T,sep=",")
+      } 
+    }
+    return(out)
+} 
