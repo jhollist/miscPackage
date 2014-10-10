f3d26c5024bd16b46c185ecba823fbebb934173d
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-10-10 11:31:07 -0400

    added download_shp function

---
+++ R/download_shp.R
@@ -0,0 +1,45 @@
+#' function to download all available shapefile files from a URL
+#' @export
+download_shp<-function(shape_url,layer,outfile=layer)
+{
+  #written by: jw hollister
+  #Oct 10, 2012
+  
+  #set-up/clean-up variables
+  if(length(grep("/$",shape_url))==0)
+  {
+    shape_url<-paste(shape_url,"/",sep="")
+  }
+  #creates vector of all possible shapefile extensions
+  shapefile_ext<-c(".shp",".shx",".dbf",".prj",".sbn",".sbx",
+                   ".shp.xml",".fbn",".fbx",".ain",".aih",".ixs",
+                   ".mxs",".atx",".cpg")
+  
+  #Check which shapefile files exist
+  if(require(RCurl))
+  {
+    xurl<-getURL(shape_url)
+    xlogic<-NULL
+    for(i in paste(layer,shapefile_ext,sep=""))
+    {
+      xlogic<-c(xlogic,grepl(i,xurl))
+    }
+    
+    #Set-up list of shapefiles to download
+    shapefiles<-paste(shape_url,layer,shapefile_ext,sep="")[xlogic]
+    #Set-up output file names
+    outfiles<-paste(outfile,shapefile_ext,sep="")[xlogic]   }
+  #Download all shapefiles
+  if(sum(xlogic)>0)
+  {
+    for(i in 1:length(shapefiles))
+    {
+      download.file(shapefiles[i],outfiles[i],
+                    method="auto",mode="wb")
+    }
+  } else
+  {
+    stop("An Error has occured with the input URL
+         or name of shapefile")
+  }
+  }
