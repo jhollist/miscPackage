2895b90b882ffbd31af9ab97cf10f8ea19c0b99d
Jeff Hollister <jeff.w.hollister@gmail.com>
2015-02-11 16:28:16 -0500

    added project_compare

---
+++ R/project_compare.R
@@ -0,0 +1,27 @@
+#' Compare projections
+#' 
+#' Different projections represent the distortions resulting from projection 
+#' spatial data in different ways.  This results in variation of basic 
+#' measurements that result solely from the projection chosen.  This function
+#' compares the resultant area and/or length measurements that result from 
+#' different projections.
+#' 
+#' @param insp An Spatial* object from the \code {sp} package.  Input CRS must
+#'             be defined 
+#' @param ...  spTransform arguments
+#' #@param proj1 a projection to test
+#' #@param units units used to report values
+#' @export
+project_compare<-function(insp,...){
+  #Need to test for CRS on insp
+  
+  #Check units on
+  insp2<-sp::spTransform(insp,...)
+  myList<-list(Original=list(proj=proj4string(insp),
+                             area=rgeos::gArea(insp),
+                             perim=rgeos::gLength(insp)),
+               Projected=list(proj=proj4string(insp2),
+                              area=rgeos::gArea(insp2),
+                              perim=rgeos::gLength(insp2)))
+  return(myList)
+}
