c552e07cef5325b6f9721e770b322007705b03e6
Jeff Hollister <jeff.w.hollister@gmail.com>
2015-06-15 10:36:35 -0400

    new and improved(?) download_shp

--- R/download_shp.R
+++ R/download_shp.R
@@ -1,14 +1,16 @@
 #' function to download all available shapefile files from a URL
-#' @import RCurl httr
+#' @import RCurl
 #' @export
 #' @examples
 #' \dontrun{
 #' download_shp("ftp://ftp.granit.sr.unh.edu/pub/GRANIT_Data/Vector_Data/Administrative_and_Political_Boundaries/d-nhsenatedists/2012","NHSenateDists2012")
 #' }
-download_shp<-function(shape_url,layer,outfile=layer)
+download_shp<-function(shape_url,layer,outfolder=".")
 {
   #written by: jw hollister
   #Oct 10, 2012
+  
+  #updated: June 15, 2015
   
   #set-up/clean-up variables
   if(length(grep("/$",shape_url))==0)
@@ -33,13 +35,19 @@ download_shp<-function(shape_url,layer,outfile=layer)
     #Set-up list of shapefiles to download
     shapefiles<-paste(shape_url,layer,shapefile_ext,sep="")[xlogic]
     #Set-up output file names
-    outfiles<-paste(outfile,shapefile_ext,sep="")[xlogic]   }
+    outfiles<-paste(outfolder,"/",layer,shapefile_ext,sep="")[xlogic]   
+  }
   #Download all shapefiles
   if(sum(xlogic)>0)
   {
     for(i in 1:length(shapefiles))
     {
-      x<-suppressWarnings(GET(shapefiles[i],write_disk(outfiles[i],overwrite = TRUE)))
+      x<-suppressWarnings(httr::GET(shapefiles[i],
+                                    httr::write_disk(outfiles[i],
+                                                     overwrite = TRUE)))
+      dwnld_file <- strsplit(shapefiles[i],"/")[[1]]
+      dwnld_file <- dwnld_file[length(dwnld_file)]
+      print(paste0("Downloaded ", dwnld_file, " to ", outfiles[i],"."))
     }
   } else
   {
