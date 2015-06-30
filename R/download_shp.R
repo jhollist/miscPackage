bd0614bdef257e20b604953c5b84c0ffbd1ad6fc
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2015-06-30 14:59:22 -0400

    download_shp should work without directory listing

--- R/download_shp.R
+++ R/download_shp.R
@@ -4,6 +4,7 @@
 #' @examples
 #' \dontrun{
 #' download_shp("ftp://ftp.granit.sr.unh.edu/pub/GRANIT_Data/Vector_Data/Administrative_and_Political_Boundaries/d-nhsenatedists/2012","NHSenateDists2012")
+#' download_shp("http://jwhollister.com/iale_open_science/files","LocPt")
 #' }
 download_shp<-function (shape_url, layer, outfolder = ".") 
 {
@@ -14,14 +15,20 @@ download_shp<-function (shape_url, layer, outfolder = ".")
   shapefile_ext <- c(".shp", ".shx", ".dbf", ".prj", ".sbn", 
                      ".sbx", ".shp.xml", ".fbn", ".fbx", ".ain", ".aih", ".ixs", 
                      ".mxs", ".atx", ".cpg")
-  
-  xurl <- RCurl::getURL(shape_url)
+
   xlogic <- NULL
-  
-  for (i in paste(layer, shapefile_ext, sep = "")) {
-    xlogic <- c(xlogic, grepl(i, xurl))
+  if(substr(shape_url,1,3)=="ftp"){
+    xurl <- RCurl::getURL(shape_url)
+    for (i in paste(layer, shapefile_ext, sep = "")) {
+      xlogic <- c(xlogic, grepl(i, xurl))
+    }
+  } else if(substr(shape_url,1,4)=="http"){
+    for (i in paste(shape_url,layer, shapefile_ext, sep = "")) {
+      xlogic <- c(xlogic,httr::HEAD(i)$status==200)
+    }  
   }
   
+ 
   shapefiles <- paste(shape_url, layer, shapefile_ext, 
                       sep = "")[xlogic]
   outfiles <- paste(outfolder, "/", layer, shapefile_ext, 
