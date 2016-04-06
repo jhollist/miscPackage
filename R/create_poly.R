cd6d9541739f2e815f975250fd9533187e94b26c
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2016-04-06 15:53:39 -0400

    added create poly and a rebuild

---
+++ R/create_poly.R
@@ -0,0 +1,10 @@
+#' Interactively Create Polygon from active graphics device
+#' @param p4s proj.4 string for the new poly.  Should be taken from data showing in current graphics device 
+#' @export
+create_poly <- function(p4s=NULL){
+  coords <- data.frame(locator()) 
+  poly <- rbind(coords, coords[1, ])
+  poly <- list(Polygon(poly)) 
+  poly <- SpatialPolygons(list(Polygons(poly, ID = 1)), proj4string = CRS(p4s))
+  return(poly)
+}
