5f1d77c3b2af857813183359de1dd6dc7abc88da
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2023-05-18 13:44:09 -0400

    rebuild with docs

--- R/add_mirror_repo.R
+++ R/add_mirror_repo.R
@@ -3,7 +3,9 @@
 #' This function looks at a current git repository, and sets up tow push 
 #' remotes, as origin, to push to.  First push remote is the original origin and
 #' second is a new git repo to push to.  The new repo serves as a mirror of the
-#' original and is set up as a bare repo it is a local file.
+#' original and is set up as a bare repo.  Becuase it is a bare repo you wont be 
+#' able to browse the files and folders in the mirror location.  A clone of this 
+#' repo will however restore the files and folders at the time of the last push.
 #' 
 #' @param mirror_repo The repository that will serve as the mirror.  Maybe a 
 #'                    GitHub repo URL or a local file path.  If a local file 
@@ -27,15 +29,27 @@ add_mirror_repo <- function(mirror_repo = one_drive_mirror(), current_repo = "."
     stop(paste("The current_repo,", current_repo, "is not a git repo."))
   }
   
-  system(paste0("git clone --bare ", current_repo, ' "', mirror_repo, '"'))
+  system(paste0("git clone --bare ", current_repo, ' "', mirror_repo, '"'), 
+         ignore.stdout = TRUE, 
+         ignore.stderr = TRUE)
   
-  current_remotes <- usethis::git_remotes()$origin
-  if(is.null(current_remotes)){
+  current_remotes <- system("git remote -v", intern = TRUE)
+  if(length(current_remotes)==0){
     stop("There are no current remotes set for this repo.  Please set a legit remote.")
   }
   
-  system(paste0('git remote set-url --add --push origin "', current_remotes, '"'))
-  system(paste0('git remote set-url --add --push origin "', mirror_repo, '"'))
+  if(length(current_remotes) < 3){
+    system(paste0('git remote set-url --add --push origin "', current_remotes, '"'),
+           ignore.stdout = TRUE, 
+           ignore.stderr = TRUE)
+    system(paste0('git remote set-url --add --push origin "', mirror_repo, '"'), 
+           ignore.stdout = TRUE, 
+           ignore.stderr = TRUE)
+  } else if(length(current_remotes) >= 3){
+    system(paste0('git remote set-url --add --push origin "', mirror_repo, '"'), 
+           ignore.stdout = TRUE, 
+           ignore.stderr = TRUE)
+  }
 }
 
 #' Set up path for a OneDrive remote
